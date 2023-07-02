011f678688506f0aad4bbad3b5edae08ed2de7cd
Chris Bidlake <cbidlake@live.com>
2023-07-02 10:19:28 -0400

    Create schema extraction

---
+++ sql/dump_raw_schemas.sql
@@ -0,0 +1,14 @@
+-- dump_raw_schemas.sql -- 
+
+.echo ON
+
+SHOW ALL TABLES;
+
+DESCRIBE bike_data;
+DESCRIBE central_park_weather;
+DESCRIBE daily_citi_bike_trip_counts_and_weather;
+DESCRIBE fhv_bases;
+DESCRIBE fhv_tripdata;
+DESCRIBE fhvhv_tripdata;
+DESCRIBE green_tripdata;
+DESCRIBE yellow_tripdata;
