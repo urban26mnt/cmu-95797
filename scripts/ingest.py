b2c2b3a7a7a295804ff0a3ab65392d4eb46b4124
Chris Bidlake <cbidlake@live.com>
2023-07-06 23:06:33 -0400

    Update csv ingest to all varchar, header setting

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -14,7 +14,7 @@ db_con.sql(r"CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('data\**\
 db_con.sql(r"CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('data\**\yellow_tripdata*.parquet, filename=1');")
 
 # Load csv files
-db_con.sql(r"CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('data\bike\*.csv.gz', union_by_name=True, filename=1);")
-db_con.sql(r"CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('data\central_park_weather.csv', filename=1);")
-db_con.sql(r"CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('data\daily_citi_bike_trip_counts_and_weather.csv', filename=1);")
-db_con.sql(r"CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('data\fhv_bases.csv', filename=1);")
+db_con.sql(r"CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('data\bike\*.csv.gz', union_by_name=True, allvarchar=1, filename=1);")
+db_con.sql(r"CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('data\central_park_weather.csv', union_by_name=True, allvarchar=1, filename=1);")
+db_con.sql(r"CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('data\daily_citi_bike_trip_counts_and_weather.csv', union_by_name=True, allvarchar=1,  filename=1);")
+db_con.sql(r"CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('data\fhv_bases.csv', union_by_name=True, allvarchar=1, filename=1, header=True);")
--- sql/ingest.sql
+++ sql/ingest.sql
@@ -7,7 +7,7 @@ CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('data\**\green_tripdat
 CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('data\**\yellow_tripdata*.parquet', filename=1);
 
 -- get csv data
-CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('data\bike\*.csv.gz', union_by_name=True, sample_size=-1, filename=1);
-CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('data\central_park_weather.csv', filename=1);
-CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('data\daily_citi_bike_trip_counts_and_weather.csv', filename=1);
-CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('data\fhv_bases.csv', filename=1);
+CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('data\bike\*.csv.gz', union_by_name=True, all_varchar=1, filename=1);
+CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('data\central_park_weather.csv', union_by_name=True, all_varchar=1, filename=1);
+CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('data\daily_citi_bike_trip_counts_and_weather.csv', union_by_name=True, all_varchar=1, filename=1);
+CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('data\fhv_bases.csv', union_by_name=True, header=True, filename=True, all_varchar=1);
